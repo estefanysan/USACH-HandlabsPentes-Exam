18e4ea853862ab6d262fae99be01019092e1880e
estefanysan <estefanysan13@hotmail.com>
2022-03-03 16:24:15 -0500

    Update HandlabsExam.sh

--- HandlabsExam.sh
+++ HandlabsExam.sh
@@ -2,34 +2,60 @@
 
 ETC_HOSTS=/etc/hosts
 
-# Listado de Ayuda #
+# Help List #
 help() {
-    echo "Local PentestLab Management Script (Docker based)"
-    echo
-    echo "Usage: $0 {list|status|info|start|stop} [projectname]" >&2
-    echo
-    echo " This scripts uses docker and hosts alias to make web apps available on localhost"
-    echo 
-    echo " Ex."
-    echo " $0 list"
-    echo " 	List all available projects"
-    echo " $0 status"
-    echo "	Show status for all projects"
-    echo " $0 start bwapp"
-    echo " 	Start project and make it available on localhost" 
-    echo " $0 info bwapp"
-    echo " 	Show information about bwapp project"
-    echo
+    echo "Local PentestLab Docker based"
+    echo "Mode of use: ./HandlabsExam.sh (start|stop|status|info|list} [projectname]"
+    echo "This script use docker and hosts alias to make vulnerable web apps available on localhost"
+    echo "some examples of use are:"
+    echo " ./HandlabsExam.sh start bwapp - Start project and make it available on localhost"
+	echo " ./HandlabsExam.sh stop bwapp - Stop project available on localhost"
+    echo " ./HandlabsExam.sh status - Show status for all projects"
+    echo " ./HandlabsExam.sh info bwapp - Show information about bwapp project"
+	echo " ./HandlabsExam.sh list - List all available projects"
+	echo
     echo " Dockerfiles from:"
-    echo "  DVWA                   - Ryan Dewhurst (vulnerables/web-dvwa)"
-    echo "  Mutillidae II          - Nikolay Golub (citizenstig/nowasp)"
     echo "  bWapp                  - Rory McCune (raesene/bwapp)"
     echo "  Webgoat(s)             - OWASP Project"
-    echo "  NodeGoat		   - Ben McMahon (bjm243/nodegoat)"
-    echo "  Juice Shop             - Bjoern Kimminich (bkimminich/juice-shop)"
     echo "  Vulnerable Wordpress   - WPScan Team (l505/vulnerablewordpress)"
     echo "  Security Ninjas        - OpenDNS Security Ninjas AppSec Training"
     exit 1
 }
+
+#Check Docker status (Instaled/running)#
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo 
+  echo "Docker not found. You need install docker before running this script."
+  echo "For kali linux you can install docker with the following command:"
+  echo "  apt install docker.io"
+  exit
+fi
+
+if sudo service docker status | grep inactive > /dev/null
+then 
+	echo "Docker is not running."
+	echo -n "Do you want to start docker now (y/n)?"
+	read answer
+	if echo "$answer" | grep -iq "^y"; then
+		sudo service docker start
+	else
+		echo "Not starting. Script will not be able to run applications."
+	fi
+fi
+
+#List vulnerable apps#
+list() {
+    echo "Available pentest applications"
+    echo "  bwapp 		- bWAPP PHP/MySQL based from itsecgames.com"
+    echo "  webgoat7		- WebGoat 7.1 OWASP Flagship Project"
+    echo "  webgoat8		- WebGoat 8.0 OWASP Flagship Project"
+    echo "  vulnerablewordpress	- WPScan Vulnerable Wordpress"
+    echo "  securityninjas	- OpenDNS Security Ninjas"
+    echo
+    exit 1
+
+}
 help
+
 #Prueba de modificacion 23453
